d267de2c92d25ee05c750b50e826246c44a62b6a
Geovane <geovanebussola73@gmail.com>
2025-05-14 10:25:53 -0300

    Update identificaeclassificapalavrasunicas.py

--- Treino py/Treinos/identificaeclassificapalavrasunicas.py
+++ Treino py/Treinos/identificaeclassificapalavrasunicas.py
@@ -1,25 +1,20 @@
-import string
-texto = input('Digite um texto: ')
-contador = 0
-contador2 = 0
-brakewhile= 1
-lista = []
-for i in string.punctuation:
-        texto = texto.replace(i,"")
-texto = texto.split()
-contador = len(texto)
-while contador != 0:
-    for i in texto:
-          brakewhile=1
-          while brakewhile == 0:
-                for a in texto:
-                    if i > a:
-                          contador2+=1
-                    else:
-                          lista.insert(contador2,i)
-                          contador2= 0
-                          brakewhile = 0
+# import string
+# texto = input('Digite um texto: ')
+# contador = 0
+# for i in string.punctuation:
+#         texto = texto.replace(i,"")
+# texto = texto.split()
+# contador = len(texto)
+# texto.sort()
+# print(f'Este texto tem {contador} palavras, organizadas alfabeticamentes:{texto} ')
 
-        
 
-print(f'Este texto tem {contador} palavras, organizadas alfabeticamentes:{lista} ')
+# import string
+# texto = input('Digite um texto: ')
+# contador = 0
+# for i in string.punctuation:
+#       texto = texto.replace(i,"")
+# contador = len(texto)
+# ordenado = "".join(sorted(texto))
+
+# print(f'Este texto tem {contador} palavras, organizadas alfabeticamentes:{ordenado} ')
