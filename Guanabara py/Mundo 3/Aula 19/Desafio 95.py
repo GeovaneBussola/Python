13655e1427c264746117a62f51d449a448bf843b
Geovane <geovanebussola73@gmail.com>
2025-07-02 15:06:00 -0300

    a

--- Guanabara py/Mundo 3/Aula 19/Desafio 95.py
+++ Guanabara py/Mundo 3/Aula 19/Desafio 95.py
@@ -1,8 +1,14 @@
 # xercício Python 095: Aprimore o desafio 93 para que ele funcione com vários jogadores, incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+
+
+#lista dos dados
 jogadores = []
+
+
+# Entrada de dados
 while True:
     jogador={'Gols':[]}
-    jogador['Nome'] = input('Digite o nome do Jogador: ')
+    jogador['Nome'] = input('Digite o nome do Jogador: ').capitalize()
     jogador['Partida(s)'] = int(input(f'Digite a quantidade de partidas que o {jogador["Nome"]} jogou: '))
     for i in range(jogador['Partida(s)']):
         jogador['Gols'].append(int(input(f'Digite quantos gols ele fez na {i+1}° partida: ')))
@@ -16,7 +22,27 @@ while True:
             print('Escolha apenas S ou N')
     if continua == 'N':
         break
+print('-='*18)
+a =(f'{"Num":^6}{"Jogador":<15}{"Total":<8}{"Gols"}')
+print(a)
+print((len(a) + 2)*"-")
 
+
+#Analisar dado especifico
+for i, v in enumerate(jogadores):
+    print(f'{i:^5} {v["Nome"]:<15}{v["Total de gols"]:^8}{v["Gols"]}')
+print((len(a) + 2)*"-")
+while True:
+    acessar = int(input('Deseja fazer o levantamento de qual jogador? '))
+    if acessar == 999:
+        break
+    elif acessar < 0 or acessar >= len(jogadores):
+        print('Digite um codigo valido!')
+        continue
+    print(f'-- Levantamento do Jogador {jogadores[acessar]["Nome"]}:')
+    for i in range(len(jogadores[acessar]['Gols'])):
+        print(f'Na {i+1}° partida fez {jogadores[acessar]["Gols"][i]}')
+    
 
 
     
