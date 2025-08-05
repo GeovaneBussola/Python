c4670ce25792cac8a289dbccc495cd733d09256e
Geovane <geovanebussola73@gmail.com>
2025-08-05 15:16:49 -0300

    a

--- jogodino/dinogame.py
+++ jogodino/dinogame.py
@@ -2,6 +2,22 @@ import pygame
 from pygame.locals import *
 from sys import exit
 import os
+from random import randrange
+from random import choice
+
+pygame.init()
+pygame.mixer.init()
+
+aleatorio = choice([0,1])
+
+pontos = 0
+velocidade_jogo = 10
+
+def exibe_mensagem(msg,tamanho,cor):
+    fonte = pygame.font.SysFont('comicsansms',tamanho,True,False)
+    mensagem = f'{msg}'
+    texto_formatado = fonte.render(mensagem,True,cor)
+    return texto_formatado
 
 diretorio_principal = os.path.dirname(__file__)
 diretorio_imagens = os.path.join(diretorio_principal, 'imagens')
@@ -19,9 +35,19 @@ pygame.display.set_caption('Dino')
 
 sprite_sheat = pygame.image.load(os.path.join(diretorio_imagens,'dinoSpritesheet.png')).convert_alpha()
 
+som_colisao = pygame.mixer.Sound(os.path.join(diretorio_sons,'death_sound.wav'))
+som_colisao.set_volume(1)
+
+som_pontuaçao = pygame.mixer.Sound(os.path.join(diretorio_sons,'score_sound.wav'))
+som_pontuaçao.set_volume(1)
+
+colidiu = False
+
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.som_pulo = pygame.mixer.Sound(os.path.join(diretorio_sons,'jump_sound.wav'))
+        self.som_pulo.set_volume(1)
         self.imagens_dinossauro = []
         for i in range(3):
             img = self.img1 = sprite_sheat.subsurface((i * 32,0),(32,32))
@@ -30,12 +56,27 @@ class Dino(pygame.sprite.Sprite):
         self.index_lista = 0
         self.image = self.imagens_dinossauro[self.index_lista]
         self.rect = self.image.get_rect()
-        self.rect.center = (100,altura - 90)
+        self.pos_y_inicial = altura - 64 -96 //2
+        self.rect.center = (100,altura - 64)
+        self.pulo = False
+    def pular(self):
+        self.pulo = True
+        self.som_pulo.play()
     def update(self):
+        if self.pulo:
+            self.rect.y-= velocidade_jogo * 2
+            if self.rect.y <= 200:
+                self.pulo = False
+        else:
+            if self.rect.y < self.pos_y_inicial:
+                self.rect.y += velocidade_jogo *2
+            else:
+                self.rect.y = self.pos_y_inicial
         if self.index_lista > 2:
             self.index_lista = 0
         self.index_lista += 0.25
         self.image = self.imagens_dinossauro[int(self.index_lista)]
+
 
 class Nuvens(pygame.sprite.Sprite):
     def __init__(self):
@@ -43,21 +84,89 @@ class Nuvens(pygame.sprite.Sprite):
         self.image = sprite_sheat.subsurface((7*32,0),(32,32))
         self.image = pygame.transform.scale(self.image,(3*32,32*3))
         self.rect = self.image.get_rect()
-        self.rect.center = (100,100)
+        self.rect.y = randrange(50,200,50)
+        self.rect.x = largura - randrange(30,300,90)
     def update(self):
         if self.rect.topright[0] < 0:
             self.rect.x = largura
-        self.rect.x -= 10
+            self.rect.y = randrange(0,150,50)
+        self.rect.x -= 5
+
+
+class Chao(pygame.sprite.Sprite):
+    def __init__(self,pos_x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheat.subsurface((6*32,0),(32,32))
+        self.image = pygame.transform.scale(self.image,(2*32,32*2))
+        self.rect = self.image.get_rect()
+        self.rect.y = altura - 64
+        self.rect.x = pos_x * 64
+    def update(self):
+        if self.rect.topright[0] < 0:
+            self.rect.x = largura
+        self.rect.x -= velocidade_jogo
+
+
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheat.subsurface((5*32,0),(32,32))
+        self.image = pygame.transform.scale(self.image,(2*32,32*2))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.center = (largura,altura - 64)
+        self.rect.x = largura
+        self.aleatorio = aleatorio
+    def update(self):
+        if self.aleatorio == 0:
+            if self.rect.topright[0] < 0:
+                self.rect.x = largura
+            self.rect.x -= velocidade_jogo
+
+class DinoVoador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.imagens_dinossauro = []
+        for i in range(3,5):
+            img = sprite_sheat.subsurface((i*32,0),(32,32))
+            img = pygame.transform.scale(img,(32*3,32*3))
+            self.imagens_dinossauro.append(img)
+        self.index_lista = 0
+        self.image = self.imagens_dinossauro[self.index_lista]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.center = (largura,300)
+        self.rect.x = largura
+        self.aleatorio = aleatorio
+    def update(self):
+        if self.aleatorio == 1:
+            if self.rect.topright[0] < 0:
+                self.rect.x = largura
+            self.rect.x -= velocidade_jogo
+            if self.index_lista > 1:
+                self.index_lista = 0
+            self.index_lista += 0.25
+            self.image = self.imagens_dinossauro[int(self.index_lista)]
 
 todas_as_sprites = pygame.sprite.Group()
 dino = Dino()
 todas_as_sprites.add(dino)
+for i in range(4):
+    nuven = Nuvens()
+    todas_as_sprites.add(nuven)
+for i in range(20):
+    chao = Chao(i)
+    todas_as_sprites.add(chao)
+cacto = Cacto()
+todas_as_sprites.add(cacto)
+dinovoador = DinoVoador()
+todas_as_sprites.add(dinovoador)
 
-nuven = Nuvens()
-todas_as_sprites.add(nuven)
 
 relogio = pygame.time.Clock()
-
+grupo_obstaculos = pygame.sprite.Group()
+grupo_obstaculos.add(cacto)
+grupo_obstaculos.add(dinovoador)
 while True:
     relogio.tick(30)
     tela.fill(branco)
@@ -65,11 +174,46 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
-
-
+        if event.type == KEYDOWN:   
+            if event.key == K_SPACE or K_w:
+                if dino.rect.y != dino.pos_y_inicial:
+                    pass
+                else:
+                    dino.pular()
+    colisoes = pygame.sprite.spritecollide(dino,grupo_obstaculos,False,pygame.sprite.collide_mask)
+    
     todas_as_sprites.draw(tela)
-    todas_as_sprites.update()
 
+    if cacto.rect.topright[0] < 0 or dinovoador.rect.topright[0] < 0:
+        escolha_obstaculo = choice([0,1])
+        cacto.rect.x = largura
+        dinovoador.rect.x = largura
+        cacto.aleatorio = escolha_obstaculo
+        dinovoador.aleatorio = escolha_obstaculo
+
+    if colisoes and colidiu == False:
+        som_colisao.play()
+        colidiu = True
+    if colidiu:
+        if pontos % 100 == 0:
+            pontos+=1
+        gameover = exibe_mensagem('GAME OVER',40,(0,0,0))
+        tela.blit(gameover,(largura//2,altura//2))
+        pressr = exibe_mensagem('Pressione \'r\' para reiniciar',20,(0,0,0))
+        tela.blit(pressr,(largura//2,altura//2 + 60))
+
+    else:
+        pontos+=1
+        todas_as_sprites.update()
+        mensagem = exibe_mensagem(pontos,40,(0,0,0))
+    if pontos % 100 == 0:
+        som_pontuaçao.play()
+        if velocidade_jogo >= 23:
+            velocidade_jogo+=0
+        else:
+            velocidade_jogo+=1
+    
+    tela.blit(mensagem,(520,30))
     pygame.display.flip()
 
 
