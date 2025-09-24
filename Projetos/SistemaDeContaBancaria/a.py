cb80b38e7eb2ea7940342e8c65b02afa3336b78d
Geovane <geovanebussola73@gmail.com>
2025-09-23 21:30:23 -0300

    Reiniciando projeto de automação

--- Projetos/SistemaDeContaBancaria/a.py
+++
@@ -1,5 +0,0 @@
-num = int(input())
-
-for i in range(num-1,0,-1):
-    num =num * i
-print(num)
---
+++ Trabalho/Colegio_Madre_Cabrini/Novas_Matriculas.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from time import sleep
+import pandas as pd
+import os
+
+
+
+
+chrome = webdriver.Chrome()
+sleep(4)
