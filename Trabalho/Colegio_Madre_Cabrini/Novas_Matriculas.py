32a014f250095549699450898a5ea5420a6d6898
Geovane Bussola <geovane.bussola@madrecabrini.com.br>
2025-09-30 14:13:59 -0300

    Goodle e Moodle prontos

--- Trabalho/Colegio_Madre_Cabrini/Novas_Matriculas.py
+++ Trabalho/Colegio_Madre_Cabrini/Novas_Matriculas.py
@@ -33,14 +33,15 @@ def google(login_google,senha_google):
 
     while True:
         try:
-            espera.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Adicionar um usuário"]'))) # Espera e adicona novo usuario e clica
+            espera_media.until(EC.element_to_be_clickable((By.XPATH, '//span[text()="Adicionar um usuário"]'))) # Espera adiciona novo usuario e clica
             chrome.find_element(By.XPATH, '//span[text()="Adicionar um usuário"]').click()
             break
         except TimeoutException:
             chrome.refresh()
             sleep(2)
 
-    for dicionario in lista_alunos: # Cria os registros necessarios para preencher o formulario do google adm
+    # Cria os registros necessarios para preencher o formulario do google adm
+    for dicionario in lista_alunos: 
         nome_completo = dicionario['Nome']
         nome_dividido = nome_completo.split()
         primeiro_nome = nome_dividido[0]
@@ -92,58 +93,111 @@ def google(login_google,senha_google):
     chrome.get("chrome://newtab/")
 
 def moodle(login_moodle,senha_moodle):
+    def loga_moodle():
+        # Só sai do loop quando conseguir logar no moodle
+        while True:
+            try:
+                espera.until(EC.presence_of_element_located((By.ID,'username')))
+                chrome.find_element(By.ID,'username').clear()
+                chrome.find_element(By.ID,'password').clear()
+                chrome.find_element(By.ID,'username').send_keys(login_moodle)
+                chrome.find_element(By.ID,'password').send_keys(senha_moodle)
+                chrome.find_element(By.ID,'loginbtn').click()
+                espera.until(EC.presence_of_element_located((By.ID,'id_username')))
+                break
+            except TimeoutException:
+                chrome.refresh()
+
+    def adiciona_turma(dicionario,indice=0):
+            # Cria o nome do aluno com a turma para filtrar em increver usuário
+            if dicionario['Turma'][2] == 'i':
+                turma_para_filtro = f"Infantil {dicionario['Turma'][0]} - {dicionario['Turma'][4].upper()}"
+            elif dicionario['Turma'][2] == 'f':
+                turma_para_filtro = f"{dicionario['Turma'][0]} Ano {dicionario['Turma'][4].upper()}"
+            elif dicionario['Turma'][2] == 'm':
+                turma_para_filtro = f"{dicionario['Turma'][0]} Série {dicionario['Turma'][4].upper()}"
+
+            # Define o link com indice caso aluno seja do ensino médio
+            if dicionario['Turma'][2] != 'm':
+                link_para_turma = turmas_moodle[turma]
+            else:
+                link_para_turma = turmas_moodle[turma][indice]
+
+            # Clica em Inscrever usuários (Para adicionar na turma)
+            chrome.get(link_para_turma)
+            espera.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input.btn.btn-primary[value='Inscrever usuários']")))
+            sleep(1.5)
+            chrome.find_element(By.XPATH, "//input[@type='submit' and @value='Inscrever usuários']").click()
+
+            # Escreve o filtro e adiciona na turma
+            espera.until(EC.visibility_of_element_located((By.XPATH, "//input[@data-fieldtype='autocomplete']"))).send_keys(str(dicionario['RM']))
+            try:
+                espera.until(EC.element_to_be_clickable((By.XPATH, f"//li[.//span[text()='{dicionario['Nome']} {turma_para_filtro}']]"))).click()
+            except TimeoutException:
+                print(f'O aluno {dicionario['Nome']} não foi adicionado a turma')
+                pass
+            sleep(3)
+            espera.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Inscrever usuários selecionados e coortes']"))).click()
+            sleep(3.5)
+
     chrome.get('https://educacaocabriniana.com.br/moodle/user/editadvanced.php?id=-1') # Abre Moodle
-    # Loga no moodle
-    while True:
-        try:
-            espera.until(EC.presence_of_element_located((By.ID,'username')))
-            chrome.find_element(By.ID,'username').send_keys(login_moodle)
-            chrome.find_element(By.ID,'password').send_keys(senha_moodle)
-            chrome.find_element(By.ID,'loginbtn').click()
-            espera.until(EC.presence_of_element_located((By.ID,'id_username')))
-            break
-        except TimeoutException:
-            chrome.refresh()
-            espera.until(EC.presence_of_element_located((By.ID,'username')))
-            chrome.find_element(By.ID,'username').clear()
-            chrome.find_element(By.ID,'password').clear()
+    
+    loga_moodle()
 
-    for dicionario in lista_alunos: # Cria os registros necessarios para preencher o formulario do moodle
-        rm = f"mc.{dicionario['RM']}@madrecabrini.com.br"
-        nome_completo = dicionario['Nome']
-        nome_dividido = nome_completo.split()
-        senha = f"{dicionario['RM']}{nome_dividido[-1]}"
-        if dicionario['Turma'][2] == 'i':
-            turma = f"Infantil {dicionario['Turma'][0]} - {dicionario['Turma'][4].upper()}"
-        elif dicionario['Turma'][2] == 'f':
-            turma = f"{dicionario['Turma'][0]} Ano {dicionario['Turma'][4].upper()}"
-        elif dicionario['Turma'][2] == 'm':
-            turma = f"{dicionario['Turma'][0]} Série {dicionario['Turma'][4].upper()}"
+    continuar = 0 # Define variavel de ferramenta para o tratamento de erro de seção expirada
 
-        espera.until(EC.visibility_of_element_located((By.ID, "id_username"))).send_keys(str(rm))
-        
-        chrome.find_element(By.XPATH, '//a[contains(., "Clique para inserir texto")]').click()
+    # Cria os registros necessarios para preencher o formulario do moodle
+    for dicionario in lista_alunos: 
+        for i in range(2): # Esse for é um metodo para caso a sessão expire seção (logar novamente)
+            rm = f"mc.{dicionario['RM']}@madrecabrini.com.br"
+            nome_completo = dicionario['Nome']
+            nome_dividido = nome_completo.split()
+            senha = f"{dicionario['RM']}{nome_dividido[-1]}"
+            if dicionario['Turma'][2] == 'i':
+                turma = f"Infantil {dicionario['Turma'][0]} - {dicionario['Turma'][4].upper()}"
+            elif dicionario['Turma'][2] == 'f':
+                turma = f"{dicionario['Turma'][0]} Ano {dicionario['Turma'][4].upper()}"
+            elif dicionario['Turma'][2] == 'm':
+                turma = f"{dicionario['Turma'][0]} Série {dicionario['Turma'][4].upper()}"
 
-        chrome.find_element(By.ID,'id_newpassword').send_keys(senha)
-
-        chrome.find_element(By.ID,'id_firstname').send_keys(nome_completo)
-
-        chrome.find_element(By.ID,'id_lastname').send_keys(turma)
-
-        chrome.find_element(By.ID,'id_email').send_keys(rm)
-
-        criar_usuario = chrome.find_element(By.ID,'id_submitbutton')
-        chrome.execute_script("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", criar_usuario) # Scrola até criar usuario
-        espera.until(EC.element_to_be_clickable((By.ID,'id_submitbutton')))
-        criar_usuario.click()
-
-        try:
-            espera.until(EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Adicionar um novo usuário")]')))
-        except TimeoutException:
-            print(f'Não foi possivel adicionar o usuario {nome_completo} no moodle (Provavelmente gmail ja existe)')
-            chrome.get('https://educacaocabriniana.com.br/moodle/user/editadvanced.php?id=-1')
-            continue
+            # Preenchimento dos campos do formulario
+            espera.until(EC.visibility_of_element_located((By.ID, "id_username"))).send_keys(str(rm))
             
+            chrome.find_element(By.XPATH, '//a[contains(., "Clique para inserir texto")]').click()
+
+            chrome.find_element(By.ID,'id_newpassword').send_keys(senha)
+
+            chrome.find_element(By.ID,'id_firstname').send_keys(nome_completo)
+
+            chrome.find_element(By.ID,'id_lastname').send_keys(turma)
+
+            chrome.find_element(By.ID,'id_email').send_keys(rm)
+
+            # Adiciona o usuário
+            criar_usuario = chrome.find_element(By.ID,'id_submitbutton')
+            chrome.execute_script("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", criar_usuario) # Scrola até criar usuario
+            espera.until(EC.element_to_be_clickable((By.ID,'id_submitbutton')))
+            criar_usuario.click()
+
+            # Tratamento de possiveis erros (Aluno ja existe no moodle ou seção expirou)
+            try:
+                espera.until(EC.presence_of_element_located((By.XPATH, '//button[contains(text(), "Adicionar um novo usuário")]')))
+                break
+            except TimeoutException:
+                try:
+                    espera.until(EC.presence_of_element_located((By.ID,'username')))
+                    loga_moodle()
+                    continue
+                except TimeoutException:
+                    chrome.get('https://educacaocabriniana.com.br/moodle/user/editadvanced.php?id=-1')
+                    print(f'Não foi possivel adicionar o aluno {dicionario['Nome']} no moodle')
+                    continuar = 1
+                    break
+        if continuar == 1:
+            continuar = 0
+            continue
+        
+        # Se for o ultimo aluno da planilha ele pula a etapa de adiconar novo usuário
         if dicionario == lista_alunos[-1]:
             pass
         else:
@@ -151,7 +205,8 @@ def moodle(login_moodle,senha_moodle):
             chrome.execute_script("arguments[0].scrollIntoView({block: 'center', behavior: 'smooth'});", adicionar_um_novo_usuario) # Scrola até adicionar um novo usuario
             espera.until(EC.element_to_be_clickable((By.XPATH, '//button[contains(text(), "Adicionar um novo usuário")]')))
             adicionar_um_novo_usuario.click()
-
+    
+    # Lista de links para os cursos no moodle
     turmas_moodle = {'1 e 2-i-a':'https://educacaocabriniana.com.br/moodle/user/index.php?id=3157',
                      '1 e 2-i-b':'https://educacaocabriniana.com.br/moodle/user/index.php?id=3158',
                      '3-i-a':'https://educacaocabriniana.com.br/moodle/user/index.php?id=3159',
@@ -194,39 +249,29 @@ def moodle(login_moodle,senha_moodle):
                      '8-f-b':'https://educacaocabriniana.com.br/moodle/user/index.php?id=3175',
                      '9-f-a':'https://educacaocabriniana.com.br/moodle/user/index.php?id=3176',
                      '9-f-b':'https://educacaocabriniana.com.br/moodle/user/index.php?id=3177',
-                     '1-m-a':'',
-                     '2-m-a':'',
-                     '3-m-a':'',
-                     '3-m-b':''
+                     '1-m-a':('https://educacaocabriniana.com.br/moodle/user/index.php?id=3246','https://educacaocabriniana.com.br/moodle/user/index.php?id=3248'),
+                     '2-m-a':('https://educacaocabriniana.com.br/moodle/user/index.php?id=3245','https://educacaocabriniana.com.br/moodle/user/index.php?id=3249'),
+                     '3-m-a':('https://educacaocabriniana.com.br/moodle/user/index.php?id=3244','https://educacaocabriniana.com.br/moodle/user/index.php?id=3250'),
+                     '3-m-b':('https://educacaocabriniana.com.br/moodle/user/index.php?id=3244','https://educacaocabriniana.com.br/moodle/user/index.php?id=3251')
                      }
+    
+    # For para adicionar um aluno de cada vez a sua respectiva turma
     for dicionario in lista_alunos:
+        # Junção das turmas dos primeiros e segundos anos do infantil
         turma = dicionario['Turma']
         if turma in ('1-i-a','2-i-a'):
             turma = '1 e 2-i-a'
         elif turma in ('1-i-b','2-i-b'):
             turma = '1 e 2-i-b'
-
-        if dicionario['Turma'][2] == 'i':
-            turma_para_filtro = f"Infantil {dicionario['Turma'][0]} - {dicionario['Turma'][4].upper()}"
-        elif dicionario['Turma'][2] == 'f':
-            turma_para_filtro = f"{dicionario['Turma'][0]} Ano {dicionario['Turma'][4].upper()}"
-        elif dicionario['Turma'][2] == 'm':
-            turma_para_filtro = f"{dicionario['Turma'][0]} Série {dicionario['Turma'][4].upper()}"
-
-        link_para_turma = turmas_moodle[turma]
-        chrome.get(link_para_turma)
-        espera.until(EC.element_to_be_clickable((By.XPATH, "//input[@type='submit' and @value='Inscrever usuários']"))).click()
-        espera.until(EC.visibility_of_element_located((By.XPATH, "//input[@data-fieldtype='autocomplete']"))).send_keys(str(dicionario['RM']))
-        try:
-            espera.until(EC.element_to_be_clickable((By.XPATH, f"//li[.//span[text()='{dicionario['Nome']} {turma_para_filtro}']]"))).click()
-        except TimeoutException:
-            print(f'O aluno {dicionario['Nome']} não foi adicionado a turma')
-            pass
-        sleep(5)
-        espera.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Inscrever usuários selecionados e coortes']"))).click()
-        sleep(3)
         
+        # Define quantas vezes sera necessario chamar o (adicionar_turma) com base na turma do aluno
+        if dicionario['Turma'][2] != 'm':
+            adiciona_turma(dicionario)
+        else:
+            for indice in range(len(turmas_moodle[turma])):
+                adiciona_turma(dicionario,indice)
 
+        
 # Acessa a planilha
 diretorio = os.path.dirname(os.path.abspath(__file__))
 caminho_planilha = os.path.join(diretorio,'new_matricula.xlsx')
@@ -249,6 +294,8 @@ senha_moodle = ''
 
 chrome = webdriver.Chrome()
 chrome.maximize_window()
-espera = WebDriverWait(chrome,8)
+espera = WebDriverWait(chrome,12)
+espera_media = WebDriverWait(chrome,15)
 
+google(login_google,senha_google)
 moodle(login_moodle,senha_moodle)
